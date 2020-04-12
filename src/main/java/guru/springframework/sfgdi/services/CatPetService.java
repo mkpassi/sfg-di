3b69aacc3f0d58f063615f5aa5031d8313cccbe9
miteshpassi <mitesh.passi@gmail.com>
2020-04-12 22:24:30 +0530

    push

--- src/main/java/guru/springframework/sfgdi/services/CatPetService.java
+++ src/main/java/guru/springframework/sfgdi/services/CatPetService.java
@@ -1,14 +1,30 @@
 package guru.springframework.sfgdi.services;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 /**
  * Created by jt on 12/28/19.
  */
+@Primary
 @Service("cat")
-public class CatPetService implements PetService {
+public class CatPetService implements PetService, InitializingBean, DisposableBean, BeanPostProcessor {
     @Override
     public String getPetType() {
         return "Cats Are the Best!";
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+    }
+
+    @Override
+    public void destroy() throws Exception {
+
     }
 }
